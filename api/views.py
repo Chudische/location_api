5ca425a25f13f2fd890119ccebb1e469d409ee1b
= <=>
2021-05-20 16:53:19 +0300

    Add func template to views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.utils.translation import to_locale
 
-# Create your views here.
+
+
+def get_location_by_name(name):
+    """Return location instance by name"""
+    pass
+
+def get_region_by_location(location):
+    """Return region by location"""
+    pass
+
+def get_district_by_location(location):
+    """Return district by location"""
+    pass
+
+def get_settelments_in_radius(location, radius):
+    """Return all settelments in area 'radius' over location"""
+    pass
+
+

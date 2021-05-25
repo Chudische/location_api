62cb8821661de64f431fcb175eae4be067a9ee53
= <=>
2021-05-25 11:32:39 +0300

    Add PlaceAdmin; fill the base

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
-# Register your models here.
+from .models import Place
+
+class PlaceResource(resources.ModelResource):
+    class Meta:
+        model = Place
+        fields = ('id', 'parent_id', 'category', 'name')
+
+class PlaceAdmin(ImportExportModelAdmin):
+    resource_class = PlaceResource
+    list_display = ('id', 'parent_id', 'category', 'name', 'coordinates')
+
+admin.site.register(Place, PlaceAdmin)
